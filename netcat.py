8788d7899cac37af00852e5326f7dec8331f63c3
l0928h <l0928h@gmail.com>
2023-01-14 14:13:26 +0100

    add netcat.py

---
+++ netcat.py
@@ -0,0 +1,97 @@
+import argparse
+import socket
+import shlex
+import subprocess
+import sys
+import textwrap
+import threading
+
+def execute(cmd):
+    cmd = cmd.strip()
+    if not cmd:
+        return
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+    return output.decode()
+
+class NetCat:
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
+# coding send function
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+        
+        try:
+            while True:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+                if response:
+                    print(response)
+                    buffer = input('> ')
+                    buffer += '\n'
+                    self.socket.send(buffer.encode())
+        except KeyboardInterrupt:
+            print('User terminated.')
+            self.socket.close()
+            sys.exit()
+
+    def listen(self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(
+                target=self.handle, args=(client_socket,)
+            )
+            client_thread.start()
+
+    def handle(self, client_socket):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='nBHP Net Tool',
+        formatter_class=argparse.RawDescriptionHelpFormatter, 
+        epilog=textwrap.dedent('''Example:
+            netcat.py -t 192.168.1.101 -p 5555 -l -c #  命令shell模式
+            netcat.py -t 192.168.1.101 -p 5555 -l -u=mytest.txt # 上傳檔案
+            netcat.py -t 192.168.1.101 -p 5555 -l -e=\"cat /etc/passwd\"  # 執行命令
+            echo 'ABC' | ./netcat.py -t 192.168.1.101 -p 135  # 回應文字到伺服器的埠號135
+            netcat.py -t 192.168.1.101 -p 5555  # 連線到伺服器
+            '''))
+    parser.add_argument('-c', '--command', action='store_true', help='command shell')
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('-l', '--listen', action='store_true', help='listen')
+    parser.add_argument('-p', '--port',  type=int, default=5555, help='listen')
+    parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
+    parser.add_argument('-u', '--upload', help='upload file')
+    args = parser.parse_args()
+    if args.listen:
+        buffer = ''
+    else:
+        buffer = sys.stdin.read()
+
+    nc = NetCat(args, buffer.encode)
+    nc.run()
+    
